581680f158c789b078a1d2d9b25aef443d966ec8
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-08-28 03:55:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,20 +1,20 @@
 def find_element_index(array, value_to_find)
-  array.length.times do |count|
+  array.length.times { |count|
     if array[count] == value_to_find
       return count 
     end
-  end
+  }
   nil
 end
 
 def find_max_value(array)
   x = array[0]
-  array.length.times do |index|
+  array.length.times { |index|
     if array[index] > x
       x = array[index]
       return x
     end
-  end
+  }
 end
 
 
